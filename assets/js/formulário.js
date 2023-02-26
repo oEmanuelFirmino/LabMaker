c1350a58775911ccfdcc2b6bcfbc1016f7874f57
firminoemanuel08 <122304402+oEmanuelFirmino@users.noreply.github.com>
2023-02-26 20:04:11 -0300

    15

---
+++ assets/js/formulário.js
@@ -0,0 +1,13 @@
+const camposDoFormulario = document.querySelectorAll('[required]')
+
+const listaRespostas = {
+    "nome": e.target.elements["nome"].value,
+    "email": e.target.elements["email"].value,
+    "mensagem": e.target.elements["mensagem"].value
+}
+
+/******************************************************************************
+**                                                                           **
+**           Developed by Emanuel Firmino & Gabriel Cruz                     **
+**                                                                           **
+*******************************************************************************/
